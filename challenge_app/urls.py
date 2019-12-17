6c06a20a936dd73e76b78c0fb9b1ce4ab178b6a2
Ari Eckmann <ari@Aris-MacBook-Pro.local>
2019-12-17 16:02:51 -0600

    many many great HTML changes, add a purchase page, added see a purchase page, profile page

--- challenge_app/urls.py
+++ challenge_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('users/<int:user_id>', views.user_page),
     path('users/<int:user_id>/purchases/new', views.add_purchase_page),
-    path('users/<int:user_id>/purchases/create', views.create_purchase)
+    path('users/<int:user_id>/purchases/create', views.create_purchase),
+    path('users/<int:user_id>/purchases', views.user_purchases)
 ]
