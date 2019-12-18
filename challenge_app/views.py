fd8dba32a49475b8c04ca4128cfab238bfa79127
Ari Eckmann <ari@Aris-MacBook-Pro.local>
2019-12-18 14:30:28 -0600

    challenge page updates

--- challenge_app/views.py
+++ challenge_app/views.py
@@ -61,6 +61,23 @@ def create_challenge(request, user_id):
         for key, value in errors.items():
             messages.error(request, value)
         return redirect(f'/app/users/{request.session["user_id"]}/add_challenge')
-    return HttpResponse('creating a challenge')
+    this_user=User.objects.get(id=request.session['user_id'])
+
+    challenge_created=Challenge.objects.create(name=request.POST['name'], purchase_max=request.POST['purchase_max'], dollar_max=request.POST['dollar_max'])
+    challenge_created.users.add(this_user)
+ 
+    
+    return redirect(f"/app/users/{request.session['user_id']}/challenges/{challenge_created.id}")
+
+def show_challenge(request, user_id, challenge_id):
+    context ={
+        "challenge_to_show":Challenge.objects.get(id=challenge_id),
+        "user":User.objects.get(id=request.session['user_id'])
+    }
+
+    return render(request, 'challengepage.html', context)
+
+
+    
 
 
