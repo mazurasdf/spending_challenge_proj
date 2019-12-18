d2b4a8cf693d673e37eb16d54c8673d0e219b3fd
Ari Eckmann <ari@Aris-MacBook-Pro.local>
2019-12-18 15:47:45 -0600

    adding a second challenger

--- challenge_app/urls.py
+++ challenge_app/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('users/<int:user_id>/purchases', views.user_purchases),
     path('users/<int:user_id>/add_challenge', views.add_challenge),
     path('users/<int:user_id>/challenges/create', views.create_challenge),
-    path('users/<int:user_id>/challenges/<int:challenge_id>', views.show_challenge)
-    # path('users/add_challenger_to_challenge/<int:challenge_id>', views.add_challenger)
+    path('users/<int:user_id>/challenges/<int:challenge_id>', views.show_challenge),
+    path('users/add_challenger_to_challenge/<int:challenge_id>', views.add_challenger)
     ]
